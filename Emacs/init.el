e2c86f0ea64697e2dbbfc9b025df9c3c59f526f6
Andrew Shinjo <shinjo1233@gmail.com>
2026-06-15 21:13:39 -0700

    first commit

---
+++ Emacs/init.el
@@ -0,0 +1,25 @@
+;;; init.el
+
+;;; Imports
+
+(let ((config-dir (file-name-directory (or load-file-name buffer-file-name))))
+  (add-to-list 'load-path (expand-file-name "org-study" config-dir))
+  (load "org-study-api"))
+
+;;; My functions
+
+(defun andy/open-init-file ()
+  (interactive)
+  (find-file user-init-file))
+
+;;; Emacs
+
+(setq make-backup-files nil)
+(global-display-line-numbers-mode 1) ;; Always show line number
+(tool-bar-mode -1)
+
+(set-frame-parameter nil 'fullscreen 'maximized) ;; Maximize Emacs window when I open it
+
+;;; Org mode
+
+(setq org-directory (expand-file-name "~/Documents/Org"))
