4a7ed33a61fbf687a51743af15f68c50f858904a
Andrew Shinjo <shinjo1233@gmail.com>
2026-06-16 12:55:38 -0700

    Refactor dispatch table out

--- Emacs/org-study/flashcard-treecloze.el
+++ Emacs/org-study/flashcard-treecloze.el
@@ -6,4 +6,15 @@
 (defconst TREECLOZE-REPETITION-PROPERTY-PREFIX "TREECLOZE_REPETITION_")
 (defconst TREECLOZE-TAG "treecloze")
 
+(defun andy/org-study/flashcard-treecloze/save () nil)
+
+(defun andy/org-study/flashcard-treecloze/parse () nil)
+
+(defun andy/org-study/flashcard-treecloze/properties ()
+  "Returns a list of TREECLOZE property names."
+  (list TREECLOZE-DUE-PROPERTY-PREFIX
+	TREECLOZE-INTERVAL-PROPERTY-PREFIX
+	TREECLOZE-EASE-FACTOR-PROPERTY-PREFIX
+	TREECLOZE-REPETITION-PROPERTY-PREFIX))
+
 (provide 'flashcard-treecloze)
